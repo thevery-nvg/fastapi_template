4cdb54d4980c759416920997a7fba1f5c0c52e4d
box7 <theverynvg@vk.com>
2024-12-22 16:36:36 +0400

    create migration for users table add method for user database

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,14 +1,20 @@
 from collections.abc import AsyncGenerator
+from typing import TYPE_CHECKING
 
 from fastapi import Depends
-from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from fastapi_users.db import SQLAlchemyUserDatabase
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
 
 from core.models import Base
 from core.models.mixins.ID_intpk_mixin import IDIntPKMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class User(Base, IDIntPKMixin, SQLAlchemyBaseUserTable[int]):
     pass
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
