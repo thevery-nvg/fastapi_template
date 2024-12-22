66948d11bfcced9e2ba508d206c54ea759349a82
box7 <theverynvg@vk.com>
2024-12-22 17:27:19 +0400

    create users and access_tokens dbs dependencies

---
+++ src/api/dependencies/users.py
@@ -0,0 +1,12 @@
+from typing import TYPE_CHECKING
+
+from fastapi import Depends
+
+from core.models import db_helper, User
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def get_users_db(session: "AsyncSession" = Depends(db_helper.session_getter())):
+    yield User.get_db(session=session)
